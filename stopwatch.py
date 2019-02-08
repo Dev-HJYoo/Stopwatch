e1cad8d9707151940d522ab77f9b5bce6345e03a
cocopambag <jhdf1234@naver.com>
2019-02-08 20:45:11 +0900

    Docs translate
    Translate Korea into English

--- stopwatch.py
+++ stopwatch.py
@@ -1,26 +1,40 @@
-import time
+# Made in Yoo Hyeong Jun 
+# github : https://github.com/cocopambag
+# email : jhdf1234@naver.com
 
-input("시작하기 위해서 아무키나 입력하세요")
+# import time library
+import time 
+
+# time.gmtime(time.time()) is returned current time.
+
+
+input("In order to start stopwatch, Please press any key. : ")
 
 now = time.gmtime(time.time())
 
-input("끝내기 위해서 아무키나 입력하세요")
+input("In order to end stopwatch, Please press any key. : ")
 
 last = time.gmtime(time.time())
 
-    
+# hour, minutes, second
+# variable name of "minutes" is "mins" because "min" is keyword.
 hour = last.tm_hour - now.tm_hour
 mins = last.tm_min - now.tm_min
 sec = last.tm_sec - now.tm_sec
 
+# if minutes over negative number
 if mins < 0:
     hour -= 1
     mins += 60
+
+# if second over negative number    
 if sec < 0:
     mins -= 1
     sec += 60
 
-print("{} 분".format(hour*60 + mins))
-print("{} 초".format(sec))
+# Print minutes and seconds
+print("{} minutes".format(hour*60 + mins))
+print("{} seconds".format(sec))
 
+# This is wait for display
 input()
